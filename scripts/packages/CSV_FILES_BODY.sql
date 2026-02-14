8dbdf634fbadef38ef4304566de588bef080977c
pvakh <116105915+pvakh@users.noreply.github.com>
2026-02-13 23:17:24 -0500

    Update CSV_FILES_BODY.sql

--- scripts/packages/CSV_FILES_BODY.sql
+++ scripts/packages/CSV_FILES_BODY.sql
@@ -3,7 +3,7 @@ create or replace PACKAGE BODY CSV_FILES AS
 PROCEDURE initial_job  is
 
 Cursor global_run_cur is 
-      SELECT tpr.*,
+SELECT tpr.*,
   rj.record_count cnt
 FROM tape_process_run tpr,
   run_journal rj
@@ -13,10 +13,26 @@ AND Substr(NVL(tpr.csv_file_name,'X'),1,3) <> 'ORA'
 and tpr.tape_id >1
 AND rj.end_time BETWEEN Sysdate-7 AND sysdate-15/1440;
 
+-- NEW 2/13/26: Cursor for Run-level files (Consolidating all tapes for a Run)
+    CURSOR master_run_cur IS
+SELECT DISTINCT tpr.process_id, tpr.run_num
+FROM tape_process_run tpr,
+  run_journal rj
+WHERE tpr.global_run_id = rj.global_run_id
+AND NVL(tpr.RUN_record_count,0)-rj.record_count<-10
+AND Substr(NVL(tpr.csv_RUN_FILE_name,'X'),1,3) <> 'ORA'
+and tpr.tape_id >1
+AND rj.end_time BETWEEN Sysdate-7 AND sysdate-15/1440
+AND rj.process_id IN (520, 521, 220); -- <--- ADD YOUR SPECIFIC PROCESS_IDs HERE
+
 
 Begin
   for rec in global_run_cur LOOP
       csv_files.write_file(rec.global_run_id); 
+  END LOOP;
+
+  FOR m_rec IN master_run_cur LOOP
+        csv_files.write_run_file(m_rec.process_id, m_rec.run_num);
   END LOOP;
 
 End initial_job ;
@@ -32,12 +48,13 @@ Cursor global_run_cur is
     and rj.end_time <Sysdate-181;
 
  vFile_name varchar2(200);
+ vFile_name1 varchar2(200);
 
 Begin
   for rec in global_run_cur LOOP
   err_loc:='E_16';
-  Select tpr.csv_file_name
-  Into vFile_name
+  Select tpr.csv_file_name, tpr.CSV_RUN_FILE_NAME
+  Into vFile_name, vFile_name1
   from tape_process_run tpr
   where tpr.global_run_id = rec.global_run_id;
 
@@ -48,11 +65,22 @@ Begin
         When Others then
              NULL;
      end;        
+     begin
+      err_loc:='E_17';
+         UTL_FILE.FREMOVE ('CSV_DIR', vFile_name1);
+     exception
+        When Others then
+             NULL;
+     end;        
+
   err_loc:='E_18';
   Update tape_process_run
   set csv_file_name = null,
       file_record_count = null,
-      file_created_date = null
+      file_created_date = null,
+      csv_run_file_name = null,
+      run_record_count = null,
+      run_file_created_date = null
   Where global_run_id = rec.global_run_id;
 
  commit;
@@ -239,6 +267,84 @@ Exception
    Raise;
    
 end write_file;
+
+PROCEDURE write_run_file (pProcess_id NUMBER, pRun_num NUMBER) IS
+    file_handle UTL_FILE.file_type;
+    vFile_name varchar2(100);
+    vCSV_headers varchar2(4000);
+    vQuery varchar2(4000);
+    vFetchedRow varchar2(4000);
+    vRecord_count  number;
+    CSV_CUR sys_refcursor;
+BEGIN
+    -- Filename includes 'MASTER' so we don't overwrite tape files
+    vFile_name := 'P'||pProcess_id||'-R'||pRun_num||'-MASTER_ALL_TAPES.tsv';
+
+    -- Open the file
+    file_handle := utl_file.fopen('CSV_DIR', vFile_name, 'w', 32767);
+
+    -- Get the SQL Blueprint from your DATA_PROCESS_VIEWS
+    -- We remove the 'global_run_id' filter and use 'run_id' instead
+    SELECT csv_headers, 
+           -- Note: We replace the specific global_run_id filter with run_id filter
+           replace(query_to_csv, 'global_run_id =', 'run_id = '||pRun_num|| ' --')
+    INTO vCSV_headers, vQuery
+    FROM data_process_views
+    WHERE process_id = pProcess_id
+    AND version_id = (SELECT max(version_id) FROM process_results WHERE process_id = pProcess_id);
+
+    -- Write Header
+    utl_file.put_line(file_handle, vCSV_headers);
+
+    -- Execute and Write Data
+    OPEN CSV_CUR FOR vQuery;
+    LOOP
+        FETCH CSV_CUR INTO vFetchedRow;
+        EXIT WHEN CSV_CUR%NOTFOUND;
+        
+        utl_file.put_line(file_handle, vFetchedRow);
+        vRecord_count:=csv_cur%rowcount;
+        
+    END LOOP;
+    CLOSE CSV_CUR;
+
+    utl_file.fclose(file_handle);
+
+ err_loc:='E_13';
+  Update tape_process_run
+  set csv_run_file_name = vFile_name,
+      run_record_count = vRecord_count-1,
+      run_file_created_date = sysdate
+  Where process_id =pProcess_id
+    and run_num=pRun_num;
+   err_loc:='E_14';
+  INSERT into audit_log (event_ts, event, event_log)
+   Values(SYSTIMESTAMP, 'CSV RUN FILE CREATED', 'CSV RUN FILE '||vFile_Name||' is done');
+  commit;
+  
+  UTL_FILE.FCLOSE_ALL;
+Exception 
+      When others then  
+   sql_err:=SUBSTR(SQLERRM, 1, 200);
+      
+
+   insert into Email_Alerts (alert, Subject, addrto, alert_text, addrfrom)
+   values ('CSV RUN FILE ERROR', 'Superox: CSV RUN FILE ERROR', 'pvakh@vpinformatics.com',
+   'CSV RUN file generated error for run file :'||vFile_Name||'. Error:'||sql_err||' '||err_loc, 'pvakh@vpinformatics.com');
+   
+   sql_err:=SUBSTR(sql_err, 1, 99);
+   err_loc:='E_15';
+   Update tape_process_run
+   set csv_run_file_name = sql_err
+   Where process_id =pProcess_id
+    and run_num=pRun_num;
+   
+  UTL_FILE.FCLOSE_ALL; 
+   
+   commit;
+   
+   Raise;
+END write_run_file;
 
 Procedure extract_IBAD_DATA is 
   vRecord_count number;
