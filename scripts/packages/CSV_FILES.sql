90a40ab81240e197fb88cbaedc10cb5248b959bd
pvakh <116105915+pvakh@users.noreply.github.com>
2026-02-13 23:17:40 -0500

    Update CSV_FILES.sql

--- scripts/packages/CSV_FILES.sql
+++ scripts/packages/CSV_FILES.sql
@@ -3,6 +3,7 @@ create or replace PACKAGE CSV_FILES AS
   PROCEDURE initial_job ;  
   PROCEDURE Delete_job ;  
   Procedure write_file (pGlobal_run_id Number);
+  PROCEDURE write_run_file (pProcess_id NUMBER, pRun_num NUMBER);
   Procedure extract_IBAD_DATA;
 
   Procedure recreate_IBAD_TABLES;
